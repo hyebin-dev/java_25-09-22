5b7a7f243d03dcc0f101206e10ceb7169ed21a5e
김혜빈 <rlagpqls0322@gmail.com>
2025-09-23 21:18:27 +0900

    Update Customer.java

--- src/day16/Customer.java
+++ src/day16/Customer.java
@@ -1,4 +1,46 @@
 package day16;
 
-public class Customer {
+public class Customer implements Comparable<Customer> {
+	private String name;
+	private int age;
+	private int price;
+	
+	public Customer() {}
+	public Customer(String name, int age) {
+		this.name = name;
+		this.age = age;
+		this.price = age >=15 ? 100 : 80;
+	}
+	
+	@Override
+	public int compareTo(Customer o) {
+		// this와 o를 비교 => 내림차순구현
+		return o.getName().compareTo(this.getName());
+	}
+	
+	@Override
+	public String toString() {
+		return "[name:" + name + ", age:" + age + ", price:" + price + "]";
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	
 }
